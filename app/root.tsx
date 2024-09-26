c73e43ba5a7fb59b104baacdb20af4243cd7a550
https://github.com/PriyanshuGupta28 <28priyanshu2001@gmail.com>
2024-09-26 13:52:45 +0530

    Update root.tsx

--- app/root.tsx
+++ app/root.tsx
@@ -6,8 +6,10 @@ import {
   ScrollRestoration,
 } from "@remix-run/react";
 import type { LinksFunction } from "@remix-run/node";
-
 import "./tailwind.css";
+import { Toaster } from "react-hot-toast";
+import Navbar from "./components/Navbar/Navbar";
+import Footer from "./components/Footer/Footer";
 
 export const links: LinksFunction = () => [
   { rel: "preconnect", href: "https://fonts.googleapis.com" },
@@ -32,7 +34,10 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <Links />
       </head>
       <body>
+        <Navbar />
         {children}
+        <Footer />
+        <Toaster position="bottom-right" reverseOrder={false} />
         <ScrollRestoration />
         <Scripts />
       </body>
